ce14e0a222cd327285c6e5818eb59e60c39adfc4
lerii <lerii.5052@gmail.com>
2025-04-01 02:54:04 +0530

    explore pages

---
+++ models/products.js
@@ -0,0 +1,45 @@
+// models/product.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');  // Adjust this path as needed
+
+const Product = sequelize.define('Product', {
+    product_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',  // Referring to the 'users' table
+            key: 'id'
+        }
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+});
+
+module.exports = Product;
